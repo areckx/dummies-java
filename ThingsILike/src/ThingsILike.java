dd59f67b2df598484b605b755af18333fa3ab559
areckx <alex.loranger.1988@gmail.com>
2018-06-07 21:35:35 -0700

    Init

---
+++ ThingsILike/src/ThingsILike.java
@@ -0,0 +1,13 @@
+/* 
+ * A program to list the good things in life
+ * Author: Barry Burd, BegingProg2@BurdBrain.com
+ * February 13, 2005
+ */
+
+public class ThingsILike {
+	
+	public static void main(String args[]) {
+
+		System.out.println("Chocolate, royalties, sleep");
+	} 
+}
