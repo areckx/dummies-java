1a55ed0225fbe27e545b3ee705ee83e1d1f5afe4
areckx <alex.loranger.1988@gmail.com>
2018-06-08 23:33:11 -0700

    Notes to self

--- ThingsILike/src/ThingsILike.java
+++ ThingsILike/src/ThingsILike.java
@@ -14,7 +14,18 @@ public class ThingsILike {
 
 
 
+// !! MUST REMEMBER !!
+// 
+//
+//	When I had first seen this block of text, I thought that I had
+//	found actual java code, but the author explained in the next paragraph
+//	that it was all fake! 
+//
 
+
+// TODO: Understand the difference between java code
+// 	     but do not worry about it this early on in your
+//		 learning of the programming concepts.
 
 // FixTheAlternator
 
