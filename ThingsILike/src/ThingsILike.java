e582eaab5fd449c843af3b6442d54e455c95a529
areckx <alex.loranger.1988@gmail.com>
2018-06-11 20:03:22 -0700

    Formatting

--- ThingsILike/src/ThingsILike.java
+++ ThingsILike/src/ThingsILike.java
@@ -32,22 +32,21 @@ public class ThingsILike {
 	public static void printSleep() {
 		System.out.println("Sleep");
 	}
-}
+} 
 
-	
+/* areckx: !! MUST REMEMBER !!
+ * 
+ *
+ *	When I had first seen this block of text, I thought that I had
+ *	found actual java code, but the author explained in the next paragraph
+ *	that it was all fake! 
+ */
 
-// !! MUST REMEMBER !!
-// 
-//
-//	When I had first seen this block of text, I thought that I had
-//	found actual java code, but the author explained in the next paragraph
-//	that it was all fake! 
-//
-
-
-// TODO: Understand the difference between java code
-// 	     but do not worry about it this early on in your
-//		 learning of the programming concepts.
+/* areckx: !TODO! Understand the difference between java code
+ * 	     but do not worry about it this early on in your
+ *		 learning of the programming concepts.
+ * 
+ */
 
 // FixTheAlternator
 
