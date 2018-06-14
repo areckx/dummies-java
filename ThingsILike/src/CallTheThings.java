995c4f71af2242539ab67e3a3c7f7d54e9458445
areckx <alex.loranger.1988@gmail.com>
2018-06-13 20:05:01 -0700

    Adds new class SnitSoft with function scamPrice()

--- ThingsILike/src/CallTheThings.java
+++ ThingsILike/src/CallTheThings.java
@@ -8,20 +8,34 @@
 public class CallTheThings {
 	public static void main(String args[]) {
 
-		ThingsILike.printThings(); 
+		//ThingsILike.printThings(); 
 		
-		ThingsILike.printChocolate();
+		//ThingsILike.printChocolate();
 		
-		ThingsILike.printRoyalties();
+		//ThingsILike.printRoyalties();
 		
-		ThingsILike.printSleep(); 
+		//ThingsILike.printSleep(); 
 		
-		EchoText.echoLine();
+		//EchoText.echoLine();
 		
-		EchoText.firstWord();
+		//EchoText.firstWord();
 		
-		EchoText.plainInt();
+		//EchoText.plainInt();
 		
+		/* areckx: unimplemented
+		 * 
+		 * I am going to go ahead and ignore this function for now. The functions above work
+		 * just fine and so I am commenting them out while I continue working through the book.
+		 * While I suppose I could make a new main class for each new example class, my goal
+		 * is to keep them modular. 
+		 * 
+		 * At the end of the book, I want to design a menu that will allow the user who runs
+		 * the program to choose which class(es) to run and possibly some other nifty options
+		 * I'll come up with when the time comes.
+		 * 
+		 */
 		//EchoText.grepChar(); 
+
+		SnitSoft.scamPrice();
 	} 
 }
---
+++ ThingsILike/src/SnitSoft.java
@@ -0,0 +1,15 @@
+
+public class SnitSoft {
+
+	public static void scamPrice() {
+		
+		double amount;
+		
+		amount = 5.95;
+		amount += 25.00;
+		
+		System.out.print("We will bill $");
+		System.out.print(amount);
+		System.out.println(" to your credit card."); 
+	} 
+}
