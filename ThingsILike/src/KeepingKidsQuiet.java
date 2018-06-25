bcfd15d51fb2149eb3417922d41972ac81d0647a
areckx <alex.loranger.1988@gmail.com>
2018-06-25 08:30:17 -0700

    always display 2 decimals

--- ThingsILike/src/KeepingKidsQuiet.java
+++ ThingsILike/src/KeepingKidsQuiet.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 
 public class KeepingKidsQuiet {
+	
+	private static DecimalFormat cents;
+	private static Scanner howMany;
 
 	public static void divideGumballs() { 
-		Scanner howMany = new Scanner(System.in);
+		cents = new DecimalFormat("0.00");
+		howMany = new Scanner(System.in);
 	
 		int gumballs;
 		int kids;
 		int gumballsPerKid;
+
+		double costOfGumballs;
+		double totalPrice;
 		
 		// Separate by a blank space
-		System.out.print("How many gumballs? How many kids? ");
+		System.out.print("How many gumballs? How many kids? Cost of each gumball? ");
 		
 		gumballs = howMany.nextInt();
 		kids = howMany.nextInt();
+		costOfGumballs = howMany.nextDouble();
 		
+		totalPrice = ((double)gumballs * costOfGumballs);
 		gumballsPerKid = (gumballs / kids);
+
 		
-		System.out.print("Each kid gets ");
-		System.out.print(gumballsPerKid);
-		System.out.println(" gumballs.");
+		System.out.print("Each kid gets " + gumballsPerKid + " gumballs ");
+		
+		System.out.println("and it's gonna cost you $" + cents.format(totalPrice)
+				+ " so no haircut for you.");
 	}
 }
